9e874c69f851255077c79e6dace2fc8eda9bcffa
Niladri <rickyrickado14@gmail.com>
2023-04-23 00:43:28 +0530

    moved images to assets

--- client/components/ProfileCard.jsx
+++ client/components/ProfileCard.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function ProfileCard(props) {
     return (
         <div className={styles.profileCard}>
-            <div className={styles.cover} style={{ backgroundImage: `url(${props.cover})` }}></div>
+            <div className={styles.cover} style={{ backgroundImage: `url(${props.cover})`}}></div>
             <Image className={styles.image} src={`/${props.pic}.jpg`} height='60' width='60' alt="profile-image" />
             <h3 className={styles.name}>{props.name} &nbsp;&nbsp;  </h3>
             <p className={styles.bio}>{props.bio}</p>
