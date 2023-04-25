b82f3b2e39b16b71fde5cf3a918b93d5deda41c2
Niladri <rabinsphotography@gmail.com>
2023-04-25 16:56:01 +0530

    updated

--- client/pages/faqs.jsx
+++ client/pages/faqs.jsx
@@ -2,9 +2,9 @@ import Navbar from '@/components/Navbar'
 import styles from '../styles/Faqs.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 function faqs() {
-
   function privacyShow() {
     const general = document.getElementById(styles.general);
     const privacy = document.getElementById(styles.privacy);
@@ -17,7 +17,7 @@ function faqs() {
     const generalTab = document.getElementById(styles.generalTab);
     const privacyTab = document.getElementById(styles.privacyTab);
     const servicesTab = document.getElementById(styles.servicesTab);
-    
+
     generalTab.style.color = 'black';
     privacyTab.style.color = '#00aaff';
     servicesTab.style.color = 'black';
@@ -35,7 +35,7 @@ function faqs() {
     const generalTab = document.getElementById(styles.generalTab);
     const privacyTab = document.getElementById(styles.privacyTab);
     const servicesTab = document.getElementById(styles.servicesTab);
-    
+
     generalTab.style.color = '#00aaff';
     privacyTab.style.color = 'black';
     servicesTab.style.color = 'black';
@@ -53,19 +53,21 @@ function faqs() {
     const generalTab = document.getElementById(styles.generalTab);
     const privacyTab = document.getElementById(styles.privacyTab);
     const servicesTab = document.getElementById(styles.servicesTab);
-    
+
     generalTab.style.color = 'black';
     privacyTab.style.color = 'black';
     servicesTab.style.color = '#00aaff';
   }
-
 
   return (
     <div className={styles.faqs}>
       <Navbar />
       <div className={styles.body}>
         <h1 className={styles.heading}>Questions? Look here.</h1>
-        <p className={styles.subHeading}>Can't find an Answer contact us on fipezocare@gmail.com</p>
+        <div className={styles.container}>
+          <Link className={styles.subHeading} href='/contact'>Can't find an Answer contact us on fipezocare@gmail.com</Link>
+          <hr className={styles.divider} />
+        </div>
         <div className={styles.content}>
           <div className={styles.categories}>
             <h1 className={styles.top}>Table of Content</h1>
@@ -77,53 +79,52 @@ function faqs() {
           </div>
           <ul className={styles.qnas} id={styles.general}>
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; Why Fipezo?
+              &nbsp; &nbsp; <h1 className={styles.summary}>Why Fipezo?</h1>
+              <p className={styles.details}>
+                Fipezo is a user-friendly platform that allows clients to easily find and connect with skilled freelancers from various fields
+              </p>
             </li>
             <hr className={styles.divider} />
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; How to Register as an Freelancer?
+              &nbsp; &nbsp; <h1 className={styles.summary}>What is Freelancing?</h1>
             </li>
             <hr className={styles.divider} />
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; How to Register as an Company?
+              &nbsp; &nbsp; <h1 className={styles.summary}>How to Register as an Freelancer?</h1>
             </li>
             <hr className={styles.divider} />
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; What is Freelancing?
+              &nbsp; &nbsp; <h1 className={styles.summary}>How to Register as an Company?</h1>
             </li>
           </ul>
           <ul className={styles.qnas} id={styles.privacy}>
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; Privacy ipsum skr tugst ghs gguisghhst ghsg?
+              &nbsp; &nbsp; <h1 className={styles.summary}>How is my Privacy of my data safe?</h1>
             </li>
             <hr className={styles.divider} />
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; Lorem ipsum skr tugst ghs gguisghhst ghsg?
+              &nbsp; &nbsp; <h1 className={styles.summary}>What sensitive information does Fipezo collect?</h1>
             </li>
             <hr className={styles.divider} />
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; Lorem ipsum skr tugst ghs gguisghhst ghsg?
+              &nbsp; &nbsp; <h1 className={styles.summary}>what information does Fipezo discloses?</h1>
             </li>
             <hr className={styles.divider} />
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; Lorem ipsum skr tugst ghs gguisghhst ghsg?
+              &nbsp; &nbsp; <h1 className={styles.summary}>Whom is the data disclosed to?</h1>
             </li>
           </ul>
           <ul className={styles.qnas} id={styles.services}>
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; Services ipsum skr tugst ghs gguisghhst ghsg?
+              &nbsp; &nbsp; <h1 className={styles.summary}>What Service does Fipezo provide?</h1>
             </li>
             <hr className={styles.divider} />
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; Lorem ipsum skr tugst ghs gguisghhst ghsg?
+              &nbsp; &nbsp; <h1 className={styles.summary}>What are the fees associated with your services?</h1>
             </li>
             <hr className={styles.divider} />
             <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; Lorem ipsum skr tugst ghs gguisghhst ghsg?
-            </li>
-            <hr className={styles.divider} />
-            <li><FontAwesomeIcon icon={faPlus} style={{ color: "#00aaff" }} />
-              &nbsp; &nbsp; Lorem ipsum skr tugst ghs gguisghhst ghsg?
+              &nbsp; &nbsp; <h1 className={styles.summary}>What are the accepted payment methods?</h1>
             </li>
           </ul>
         </div>
