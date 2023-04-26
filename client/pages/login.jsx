22f34b4a415887acb305f272060e5ed1c15437fd
Niladri <rickyrickado14@gmail.com>
2023-04-26 22:18:48 +0530

    fixed styles

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -1,39 +1,37 @@
 import Navbar from '@/components/Navbar';
 import Image from 'next/image';
 import Link from 'next/link';
-// import styles from '@/styles/Login.module.css';
+import styles from '@/styles/Login.module.css';
 
 export default function login() {
   return (
-    <>
-    </>
-    // <div className={styles.login}>
-    //   <div className={styles.navbar}>
-    //     <Navbar color='black' />
-    //   </div>
-    //   <div className={styles.body}>
-    //     <form method="post" action='/login' className={styles.form}>
-    //       <div>
-    //         <h1 className={styles.heading}>Welcome</h1>
-    //         <p className={styles.subHeading}>Log In To Your Account</p>
-    //       </div>
-    //       <div id={styles.phone}>
-    //         <div className={styles.countryCode}>
-    //           +91
-    //         </div>
-    //         <input className={styles.inputs} id={styles.number} type='number' placeholder='Enter Your Phone no.' /> <br />
-    //       </div>
-    //       <div>
-    //         <button className={styles.btn}>Send OTP</button>
-    //       </div>
-    //       <div className={styles.lower}>
-    //         <Link href='/signup' className={styles.signup}>Don&apos;t have an Account? Sign up now</Link>
-    //       </div>
-    //     </form>
-    //     <div className={styles.presentation}>
-    //       <Image src="/pre.jpg" alt="image" height="1006" width="1000" />
-    //     </div>
-    //   </div>
-    // </div>
+    <div className={styles.login}>
+      <div className={styles.navbar}>
+        <Navbar color='black' />
+      </div>
+      <div className={styles.body}>
+        <form method="post" className={styles.form}>
+          <div>
+            <h1 className={styles.heading}>Welcome</h1>
+            <p className={styles.subHeading}>Log In To Your Account</p>
+          </div>
+          <div id={styles.phone}>
+            <div className={styles.countryCode}>
+              +91
+            </div>
+            <input className={styles.inputs} id={styles.number} type='number' placeholder='Enter Your Phone no.' /> <br />
+          </div>
+          <div>
+            <button className={styles.btn}>Send OTP</button>
+          </div>
+          <div className={styles.lower}>
+            <Link href='/signup' className={styles.signup}>Don&apos;t have an Account? Sign up now</Link>
+          </div>
+        </form>
+        <div className={styles.presentation}>
+          <Image src="/pre.jpg" alt="image" height="1006" width="1000" />
+        </div>
+      </div>
+    </div>
   )
 }
