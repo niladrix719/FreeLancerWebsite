74308a40a1a7f59605906b3384979d74e46eac3d
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2023-04-28 22:57:14 +0530

    Update company.jsx

--- client/pages/register/company.jsx
+++ client/pages/register/company.jsx
@@ -13,11 +13,6 @@ export default function company() {
       {/* <div className={styles.left}>
           <h1 className={styles.heading}>What is your Profession?</h1>
           <p className={styles.subHeading}>Choose any one category to begin with.</p>
-          <div className={styles.categories}>
-            <div className={styles.category} id={styles.photographer}>Photographer</div>
-            <div className={styles.category} id={styles.cinematographer}>Cinematographer</div>
-            <div className={styles.category} id={styles.drone_operator}> Drone Operator</div>
-          </div>
         </div> */}
         <div className={styles.right}>
           <div className={styles.title}>
