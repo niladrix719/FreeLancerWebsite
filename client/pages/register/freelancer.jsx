3cca853aeac92ce6aa4fd1093b42fb44244e72dd
Niladri <rickyrickado14@gmail.com>
2023-05-01 11:31:54 +0530

    fixed validation

--- client/pages/register/freelancer.jsx
+++ client/pages/register/freelancer.jsx
@@ -13,11 +13,10 @@ class Freelancer extends React.Component {
       progress: 0,
       currentPage: 1,
       btn: 'Next',
-      btnType: 'button',
       firstName: '',
       lastName: '',
       phone: '',
-      profession: '',
+      profession: 'photographer',
       bio: '',
       equipments: '',
       error: false
@@ -25,43 +24,43 @@ class Freelancer extends React.Component {
   }
 
   increProgress = (val) => {
-    if (this.state.progress + val > 125){
+    if (this.state.progress + val > 125) {
       return;
     }
 
-    if((this.state.firstName === '' || this.state.lastName === '') && this.state.currentPage === 1){
-      this.setState({error: true});
-      return;
-    }
-    
-    if(this.state.phone === '' && this.state.currentPage === 2){
-      this.setState({error: true});
+    if ((this.state.firstName === '' || this.state.lastName === '') && this.state.currentPage === 1) {
+      this.setState({ error: true });
       return;
     }
 
-    if(this.state.profession === '' && this.state.currentPage === 3){
-      this.setState({error: true});
+    if (this.state.phone === '' && this.state.currentPage === 2) {
+      this.setState({ error: true });
       return;
     }
 
-    if(this.state.bio === '' && this.state.currentPage === 4){
-      this.setState({error: true});
+    if (this.state.profession === '' && this.state.currentPage === 3) {
+      this.setState({ error: true });
       return;
     }
 
-    if(this.state.equipments === '' && this.state.currentPage === 5){
-      this.setState({error: true});
+    if (this.state.bio === '' && this.state.currentPage === 4) {
+      this.setState({ error: true });
       return;
     }
 
-    if(this.state.currentPage === 5){
-      this.setState({error: false});
+    if (this.state.equipments === '' && this.state.currentPage === 5) {
+      this.setState({ error: true });
+      return;
+    }
+
+    if (this.state.currentPage === 5) {
+      this.setState({ error: false });
       this.handleSubmit();
       return;
     }
 
     this.setState({ progress: this.state.progress + val });
-    this.setState({error: false});
+    this.setState({ error: false });
     this.increPage();
   }
 
@@ -80,7 +79,6 @@ class Freelancer extends React.Component {
 
     if (this.state.currentPage === 4) {
       this.setState({ btn: 'Submit' });
-      this.setState({ btnType: 'submit' });
     }
 
     this.setState({ currentPage: this.state.currentPage + 1 });
@@ -92,7 +90,6 @@ class Freelancer extends React.Component {
 
     if (this.state.currentPage === 5) {
       this.setState({ btn: 'Next' });
-      this.setState({ btnType: 'button' });
     }
 
     this.setState({ currentPage: this.state.currentPage - 1 });
@@ -136,48 +133,64 @@ class Freelancer extends React.Component {
             <form className={styles.form}>
               {this.state.error && <p className={styles.error}>Please provide all the inputs the fields.</p>}
               {this.state.currentPage === 1 && <div className={styles.inputField} id={styles.firstname}>
-                <label htmlFor="firstname" className={styles.label}><span style={{color : 'red'}}>* </span>First name :</label>
+                <label htmlFor="firstname" className={styles.label}><span style={{ color: 'red' }}>* </span>First name :</label>
                 <input type='text' className={styles.input}
                   placeholder='Enter Your First name'
                   name='firstname' id='firstname' required
-                  onChange={(event) => this.setState({ firstName: event.target.value })} 
+                  onChange={(event) => this.setState({ firstName: event.target.value })}
+                  value={this.state.firstName !== '' ? this.state.firstName : ''}
+                  maxLength={15}
                 />
               </div>}
               {this.state.currentPage === 1 && <div className={styles.inputField} id={styles.lastname}>
-                <label htmlFor="lastname" className={styles.label}><span style={{color : 'red'}}>* </span>Last name :</label>
+                <label htmlFor="lastname" className={styles.label}><span style={{ color: 'red' }}>* </span>Last name :</label>
                 <input type='text' className={styles.input}
                   placeholder='Enter Your Last name'
                   name='lastname' id='lastname' required
-                  onChange={(event) => this.setState({ lastName: event.target.value })} 
+                  onChange={(event) => this.setState({ lastName: event.target.value })}
+                  value={this.state.lastName !== '' ? this.state.lastName : ''}
+                  maxLength={15}
                 />
               </div>}
               {this.state.currentPage === 2 && <div className={styles.inputField} id={styles.phone}>
-                <label htmlFor="phone" className={styles.label}><span style={{color : 'red'}}>* </span>Phone :</label>
-                <input type='number' id={styles.number}className={styles.input}
+                <label htmlFor="phone" className={styles.label}><span style={{ color: 'red' }}>* </span>Phone :</label>
+                <input type='number' id={styles.number} className={styles.input}
                   placeholder='Enter Your Phone no.'
                   name='phone' required
                   onChange={(event) => this.setState({ phone: event.target.value })}
-                  max='10' min='10'
+                  value={this.state.phone !== 'photographer' ? this.state.phone : 'photographer'}
+                  max={10}
                 />
               </div>}
               {this.state.currentPage === 3 && <div className={styles.inputField} id={styles.profession}>
-                <label htmlFor="profession" className={styles.label}><span style={{color : 'red'}}>* </span>What is your profession?</label>
-                <select required className={styles.options} name="profession" onChange={(event) => this.setState({ profession: event.target.value })} id="profession">
+                <label htmlFor="profession" className={styles.label}><span style={{ color: 'red' }}>* </span>What is your profession?</label>
+                <select required className={styles.options} name="profession"
+                  onChange={(event) => this.setState({ profession: event.target.value })} id="profession"
+                  value={this.state.profession !== '' ? this.state.profession : ''}
+                >
                   <option className={styles.option} value="photographer">Photographer</option>
                   <option className={styles.option} value="cinematographer">Cinematographer</option>
                   <option className={styles.option} value="drone_operator">Drone Operator</option>
                 </select>
               </div>}
               {this.state.currentPage === 4 && <div className={styles.inputField} id={styles.bio}>
-                <label htmlFor="bio" className={styles.label}><span style={{color : 'red'}}>* </span>Bio :</label>
-                <textarea required name="bio" id="bio" cols="30" rows="10" onChange={(event) => this.setState({ bio: event.target.value })} className={styles.textarea} placeholder='Write Your bio here...'></textarea>
+                <label htmlFor="bio" className={styles.label}><span style={{ color: 'red' }}>* </span>Bio :</label>
+                <textarea required name="bio" id="bio" cols="30" rows="10"
+                  onChange={(event) => this.setState({ bio: event.target.value })}
+                  className={styles.textarea} placeholder='Write Your bio here...'
+                  value={this.state.bio !== '' ? this.state.bio : ''}>
+                </textarea>
               </div>}
               {this.state.currentPage === 5 && <div className={styles.inputField} id={styles.equipment}>
-                <label htmlFor="equipments" className={styles.label}><span style={{color : 'red'}}>* </span>Equipments Available :</label>
-                <textarea required name="equipments" id="equipments" cols="30" rows="10" onChange={(event) => this.setState({ equipments: event.target.value })} className={styles.textarea} placeholder='Write Your equipments here...'></textarea>
+                <label htmlFor="equipments" className={styles.label}><span style={{ color: 'red' }}>* </span>Equipments Available :</label>
+                <textarea required name="equipments" id="equipments" cols="30" rows="10"
+                  onChange={(event) => this.setState({ equipments: event.target.value })}
+                  className={styles.textarea} placeholder='Write Your equipments here...'
+                  value={this.state.equipments !== '' ? this.state.equipments : ''}>
+                </textarea>
               </div>}
               <div className={styles.btns}>
-                <button className={styles.NextBtn} type={this.state.btnType} onClick={() => this.increProgress(25)}>{this.state.btn}</button>
+                <button className={styles.NextBtn} type='button' onClick={() => this.increProgress(25)}>{this.state.btn}</button>
                 <button className={styles.backBtn} type='button' onClick={() => this.decreProgress(25)}>Back</button>
               </div>
             </form>
