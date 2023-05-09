8158fe371161dbc5e92b2d81012f268aa9630aa1
Niladri <rickyrickado14@gmail.com>
2023-05-09 17:07:21 +0530

    added explore pages navigation

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -4,105 +4,105 @@ import { faChevronDown, faStar } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
 class Sidebar extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			showDropDown: true,
-			showDropDownRating: true,
-			showDropDownLocation: true,
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDropDown: true,
+      showDropDownRating: true,
+      showDropDownLocation: true,
+    };
+  }
 
-	toggle = () => {
-		this.setState((prevState) => ({
-			showDropDown: !prevState.showDropDown,
-		}));
-	};
+  toggle = () => {
+    this.setState((prevState) => ({
+      showDropDown: !prevState.showDropDown,
+    }));
+  };
 
-	toggleRating = () => {
-		this.setState((prevState) => ({
-			showDropDownRating: !prevState.showDropDownRating,
-		}));
-	};
+  toggleRating = () => {
+    this.setState((prevState) => ({
+      showDropDownRating: !prevState.showDropDownRating,
+    }));
+  };
 
-	toggleLocation = () => {
-		this.setState((prevState) => ({
-			showDropDownLocation: !prevState.showDropDownLocation,
-		}));
-	};
+  toggleLocation = () => {
+    this.setState((prevState) => ({
+      showDropDownLocation: !prevState.showDropDownLocation,
+    }));
+  };
 
-	render() {
-		return (
-			<div className={styles.sidebar}>
-				<div className={styles.filter} id={styles.category}>
-					<div className={styles.title}>
-						Category <FontAwesomeIcon onClick={this.toggle} icon={faChevronDown} className={styles.arrow} />
-					</div>
-					{this.state.showDropDown && <div className={styles.options}>
-						<div className={styles.inputs}>
-							<input className={styles.checkbox} type="checkbox" />
-							<label htmlFor="photographer">Photographer</label>
-						</div>
-						<div className={styles.inputs}>
-							<input className={styles.checkbox} type="checkbox" />
-							<label htmlFor="cinematographer">Cinematographer</label>
-						</div>
-						<div className={styles.inputs}>
-							<input className={styles.checkbox} type="checkbox" />
-							<label htmlFor="drone_operator">Drone Operator</label>
-						</div>
-					</div>}
-				</div>
+  render() {
+    return (
+      <div className={styles.sidebar}>
+        <div className={styles.filter} id={styles.category}>
+          <div className={styles.title}>
+            Category <FontAwesomeIcon onClick={this.toggle} icon={faChevronDown} className={styles.arrow} />
+          </div>
+          {this.state.showDropDown && <div className={styles.options}>
+            <div className={styles.inputs}>
+              <input className={styles.checkbox} type="checkbox" />
+              <label htmlFor="photographer">Photographer</label>
+            </div>
+            <div className={styles.inputs}>
+              <input className={styles.checkbox} type="checkbox" />
+              <label htmlFor="cinematographer">Cinematographer</label>
+            </div>
+            <div className={styles.inputs}>
+              <input className={styles.checkbox} type="checkbox" />
+              <label htmlFor="drone_operator">Drone Operator</label>
+            </div>
+          </div>}
+        </div>
 
-				<hr className={styles.divider} />
+        <hr className={styles.divider} />
 
-				<div className={styles.filter} id={styles.price}>
-					<div className={styles.title}>
-						Price
-					</div>
-					<input type='range' min='0' max='100' step='5' className={styles.slider} />
-				</div>
+        <div className={styles.filter} id={styles.price}>
+          <div className={styles.title}>
+            Price
+          </div>
+          <input type='range' min='0' max='100' step='5' className={styles.slider} />
+        </div>
 
-				<hr className={styles.divider} />
+        <hr className={styles.divider} />
 
-				<div className={styles.filter} id={styles.rating}>
-					<div className={styles.title}>
-						Customer Rating <FontAwesomeIcon onClick={this.toggleRating} icon={faChevronDown} className={styles.arrow} />
-					</div>
-					{this.state.showDropDownRating && <div className={styles.options}>
-						<div className={styles.inputs}>
-							<input className={styles.checkbox} type="checkbox" id='star4' />
-							<label htmlFor="star4">
-								4<FontAwesomeIcon icon={faStar} style={{ color: "#1f1c1c", fontSize: '15px' }} /> & Above
-							</label>
-						</div>
-						<div className={styles.inputs}>
-							<input className={styles.checkbox} type="checkbox" id='star3' />
-							<label htmlFor="star3">
-								3<FontAwesomeIcon icon={faStar} style={{ color: "#1f1c1c", fontSize: '15px' }} /> & Above
-							</label>
-						</div>
-					</div>}
-				</div>
+        <div className={styles.filter} id={styles.rating}>
+          <div className={styles.title}>
+            Customer Rating <FontAwesomeIcon onClick={this.toggleRating} icon={faChevronDown} className={styles.arrow} />
+          </div>
+          {this.state.showDropDownRating && <div className={styles.options}>
+            <div className={styles.inputs}>
+              <input className={styles.checkbox} type="checkbox" id='star4' />
+              <label htmlFor="star4">
+                4<FontAwesomeIcon icon={faStar} style={{ color: "#1f1c1c", fontSize: '15px' }} /> & Above
+              </label>
+            </div>
+            <div className={styles.inputs}>
+              <input className={styles.checkbox} type="checkbox" id='star3' />
+              <label htmlFor="star3">
+                3<FontAwesomeIcon icon={faStar} style={{ color: "#1f1c1c", fontSize: '15px' }} /> & Above
+              </label>
+            </div>
+          </div>}
+        </div>
 
-				<hr className={styles.divider} />
+        <hr className={styles.divider} />
 
-				<div className={styles.filter} id={styles.location}>
-					<div className={styles.title}>
-						Location <FontAwesomeIcon onClick={this.toggleLocation} icon={faChevronDown} className={styles.arrow} />
-					</div>
-					{this.state.showDropDownLocation && <div className={styles.options}>
-						<div className={styles.inputs}>
-							<input className={styles.checkbox} type="checkbox" id='kolkata' />
-							<label htmlFor="kolkata">
-								Kolkata
-							</label>
-						</div>
-					</div>}
-				</div>
-			</div>
-		)
-	}
+        <div className={styles.filter} id={styles.location}>
+          <div className={styles.title}>
+            Location <FontAwesomeIcon onClick={this.toggleLocation} icon={faChevronDown} className={styles.arrow} />
+          </div>
+          {this.state.showDropDownLocation && <div className={styles.options}>
+            <div className={styles.inputs}>
+              <input className={styles.checkbox} type="checkbox" id='kolkata' />
+              <label htmlFor="kolkata">
+                Kolkata
+              </label>
+            </div>
+          </div>}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Sidebar
--- client/pages/explore.jsx
+++ client/pages/explore.jsx
@@ -5,8 +5,19 @@ import Sidebar from '@/components/Sidebar';
 import { faCameraRetro, faVideo, faClapperboard } from '@fortawesome/free-solid-svg-icons';
 import SearchBox from '@/components/SearchBox';
 import Footer from '@/components/Footer';
+import { useState } from 'react';
 
 function Explore() {
+  const [currentPage, setCurrentPage] = useState(1);
+  
+  const increPage = () => {
+    setCurrentPage(currentPage + 1);
+  }
+
+  const decrePage = () => {
+    setCurrentPage(currentPage - 1);
+  }
+
   return (
     <div className={styles.explore}>
       <Navbar />
@@ -17,55 +28,212 @@ function Explore() {
         <div className={styles.sidebar}>
           <Sidebar />
         </div>
-        <div className={styles.cards}>
-          <ProfileCard
-            name="Niladri Adhikary"
-            bio="Capturing Moments with love"
-            category="Photographer"
-            pic="img01"
-            cover="/cover01.jpg"
-            icon={faCameraRetro}
-          />
-          <ProfileCard
-            name="Banhi Suresh"
-            bio="seeking to work on projects"
-            category="Cinematographer"
-            pic="img02"
-            cover="/cover02.jpg"
-            icon={faVideo}
-          />
-          <ProfileCard
-            name="Vritika Sood"
-            bio="Experienced Photographer"
-            category="Drone Operator"
-            pic="img03"
-            cover="/cover03.jpg"
-            icon={faClapperboard}
-          />
-          <ProfileCard
-            name="Richa Saini"
-            bio="Here for you"
-            category="Cinematographer"
-            pic="img04"
-            cover="/cover04.jpg"
-            icon={faVideo}
-          />
-          <ProfileCard
-            name="Nakul Nagy"
-            bio="Freeze Your Emotions"
-            category="Drone Operator"
-            pic="img05"
-            cover="/cover05.jpg"
-            icon={faClapperboard}
-          />
-          <ProfileCard
-            name="Advik Chada"
-            bio="Checkout my profile"
-            category="Photographer"
-            pic="img06"
-            cover="/cover06.jpg"
-            icon={faCameraRetro}
-          />
+        <div className={styles.main}>
+          {currentPage === 1 && <div className={styles.cards}>
+            <ProfileCard
+              name="Niladri Adhikary"
+              bio="Capturing Moments with love"
+              category="Photographer"
+              pic="img01"
+              cover="/cover01.jpg"
+              icon={faCameraRetro}
+            />
+            <ProfileCard
+              name="Banhi Suresh"
+              bio="seeking to work on projects"
+              category="Cinematographer"
+              pic="img02"
+              cover="/cover02.jpg"
+              icon={faVideo}
+            />
+            <ProfileCard
+              name="Vritika Sood"
+              bio="Experienced Photographer"
+              category="Drone Operator"
+              pic="img03"
+              cover="/cover03.jpg"
+              icon={faClapperboard}
+            />
+            <ProfileCard
+              name="Richa Saini"
+              bio="Here for you"
+              category="Cinematographer"
+              pic="img04"
+              cover="/cover04.jpg"
+              icon={faVideo}
+            />
+            <ProfileCard
+              name="Nakul Nagy"
+              bio="Freeze Your Emotions"
+              category="Drone Operator"
+              pic="img05"
+              cover="/cover05.jpg"
+              icon={faClapperboard}
+            />
+            <ProfileCard
+              name="Advik Chada"
+              bio="Checkout my profile"
+              category="Photographer"
+              pic="img06"
+              cover="/cover06.jpg"
+              icon={faCameraRetro}
+            />
+            <ProfileCard
+              name="Niladri Adhikary"
+              bio="Capturing Moments with love"
+              category="Photographer"
+              pic="img01"
+              cover="/cover01.jpg"
+              icon={faCameraRetro}
+            />
+            <ProfileCard
+              name="Banhi Suresh"
+              bio="seeking to work on projects"
+              category="Cinematographer"
+              pic="img02"
+              cover="/cover02.jpg"
+              icon={faVideo}
+            />
+            <ProfileCard
+              name="Vritika Sood"
+              bio="Experienced Photographer"
+              category="Drone Operator"
+              pic="img03"
+              cover="/cover03.jpg"
+              icon={faClapperboard}
+            />
+            <ProfileCard
+              name="Richa Saini"
+              bio="Here for you"
+              category="Cinematographer"
+              pic="img04"
+              cover="/cover04.jpg"
+              icon={faVideo}
+            />
+            <ProfileCard
+              name="Nakul Nagy"
+              bio="Freeze Your Emotions"
+              category="Drone Operator"
+              pic="img05"
+              cover="/cover05.jpg"
+              icon={faClapperboard}
+            />
+            <ProfileCard
+              name="Advik Chada"
+              bio="Checkout my profile"
+              category="Photographer"
+              pic="img06"
+              cover="/cover06.jpg"
+              icon={faCameraRetro}
+            />
+          </div>}
+          {currentPage === 2 && <div className={styles.cards}>
+            <ProfileCard
+              name="Niladri Adhikary"
+              bio="Capturing Moments with love"
+              category="Photographer"
+              pic="img01"
+              cover="/cover01.jpg"
+              icon={faCameraRetro}
+            />
+            <ProfileCard
+              name="Banhi Suresh"
+              bio="seeking to work on projects"
+              category="Cinematographer"
+              pic="img02"
+              cover="/cover02.jpg"
+              icon={faVideo}
+            />
+            <ProfileCard
+              name="Vritika Sood"
+              bio="Experienced Photographer"
+              category="Drone Operator"
+              pic="img03"
+              cover="/cover03.jpg"
+              icon={faClapperboard}
+            />
+            <ProfileCard
+              name="Richa Saini"
+              bio="Here for you"
+              category="Cinematographer"
+              pic="img04"
+              cover="/cover04.jpg"
+              icon={faVideo}
+            />
+            <ProfileCard
+              name="Nakul Nagy"
+              bio="Freeze Your Emotions"
+              category="Drone Operator"
+              pic="img05"
+              cover="/cover05.jpg"
+              icon={faClapperboard}
+            />
+          </div>}
+          {currentPage === 3 && <div className={styles.cards}>
+            <ProfileCard
+              name="Richa Saini"
+              bio="Here for you"
+              category="Cinematographer"
+              pic="img04"
+              cover="/cover04.jpg"
+              icon={faVideo}
+            />
+            <ProfileCard
+              name="Nakul Nagy"
+              bio="Freeze Your Emotions"
+              category="Drone Operator"
+              pic="img05"
+              cover="/cover05.jpg"
+              icon={faClapperboard}
+            />
+            <ProfileCard
+              name="Advik Chada"
+              bio="Checkout my profile"
+              category="Photographer"
+              pic="img06"
+              cover="/cover06.jpg"
+              icon={faCameraRetro}
+            />
+            <ProfileCard
+              name="Niladri Adhikary"
+              bio="Capturing Moments with love"
+              category="Photographer"
+              pic="img01"
+              cover="/cover01.jpg"
+              icon={faCameraRetro}
+            />
+            <ProfileCard
+              name="Banhi Suresh"
+              bio="seeking to work on projects"
+              category="Cinematographer"
+              pic="img02"
+              cover="/cover02.jpg"
+              icon={faVideo}
+            />
+            <ProfileCard
+              name="Vritika Sood"
+              bio="Experienced Photographer"
+              category="Drone Operator"
+              pic="img03"
+              cover="/cover03.jpg"
+              icon={faClapperboard}
+            />
+          </div>}
+          <nav className={styles.nav}>
+            <div className={styles.pages}>
+              <button className={styles.btn} onClick={decrePage}>Back</button>
+              <div className={styles.page} id={currentPage === 1 ? `${styles.current}` : ''}
+                onClick={() => setCurrentPage(1)}
+              >1</div>
+              <div className={styles.page} id={currentPage === 2 ? `${styles.current}` : ''}
+                onClick={() => setCurrentPage(2)}
+              >2</div>
+              <div className={styles.page} id={currentPage === 3 ? `${styles.current}` : ''}
+                onClick={() => setCurrentPage(3)}
+              >3</div>
+              <button className={styles.btn} onClick={increPage}>Next</button>
+            </div>
+          </nav>
         </div>
       </div>
       <div className={styles.footer}>
