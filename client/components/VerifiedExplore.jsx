c2f086a4bf3bef8775e23d582992703d31c874fc
Niladri <rickyrickado14@gmail.com>
2023-05-16 13:05:37 +0530

    updated explore box css

--- client/components/VerifiedExplore.jsx
+++ client/components/VerifiedExplore.jsx
@@ -1,16 +1,22 @@
 import styles from '../styles/VerifiedExplore.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 
 function VerifiedExplore() {
   return (
     <div className={styles.verified}>
       <div className={styles.text}>
-        <h1 className={styles.heading}>Explore all the verified freelancers And Find the best talent for your project needs.</h1>
-        <Link className={styles.btn} href='/explore'>Explore Freelancers</Link>
+        <h1 className={styles.heading}>
+          Explore all the verified freelancers And Find the best talent for your project needs.
+        </h1>
+        <Link className={styles.btn} href='/explore'>Explore Freelancers &nbsp;&nbsp;
+          <FontAwesomeIcon icon={faArrowRightLong} id={styles.arrow} />
+        </Link>
       </div>
       <div className={styles.image}>
-      <Image src='/verified.png' width='300' height='300' alt='verified tick'/>
+        <Image src='/verified.png' width='300' height='300' alt='verified tick' />
       </div>
     </div>
   )
