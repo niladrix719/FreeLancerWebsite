d11a90c586d3aaaf5138d2e7f49dd39bcd4cc2c1
Niladri <rickyrickado14@gmail.com>
2023-05-17 17:53:10 +0530

    contact soon page created

---
+++ client/pages/contact_soon.jsx
@@ -0,0 +1,55 @@
+import Navbar from '@/components/Navbar';
+import styles from '../styles/Contact_soon.module.css';
+import Footer from '@/components/Footer';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+function Contact_soon() {
+  return (
+    <div className={styles.contactSoon}>
+      <Navbar />
+      <div className={styles.body}>
+        <div className={styles.box}>
+          <Image className={styles.image} src='/soon.png' width='1000' height='1000' />
+          <div className={styles.heading}>
+            <i id={styles.fipezo}>Fipezo</i>
+            <span className={styles.color}>Congratulations !</span> <br /> Your registration is currently undergoing verification
+            <p className={styles.subHeading}>Our Team will contact you soon .&nbsp;.&nbsp;.</p>
+            <Link href='/'><button className={styles.btn}>
+              <FontAwesomeIcon icon={faArrowLeft} id={styles.arrow} />&nbsp;&nbsp;Back to Home
+            </button></Link>
+            <div className={styles.socials}>
+              <Image className={styles.social} src='/facebook.png' width='160' height='160' />
+              <Image className={styles.social} src='/instagramC.png' width='160' height='160' />
+              <Image className={styles.social} src='/twitterC.png' width='160' height='160' />
+              <Image className={styles.social} src='/youtube.png' width='160' height='160' />
+            </div>
+            <div className={styles.backBox}>
+              
+            </div>
+          </div>
+          <div className={styles.flex}>
+            <h1 className={styles.text}>What&apos;s to do Next?</h1>
+            <div className={styles.paras}>
+              <p className={styles.para}>
+                Wait for the verification process to complete: Sit tight while our team verifies your registration details.
+                this may take up to 24 hours. You will receive a SMS once your registration is verified.
+              </p>
+              <p className={styles.para}>Prepare your Aadhaar and PAN cards: Make sure you have your valid Aadhaar and PAN cards handy.</p>
+              <p className={styles.para}>
+                Prepare a conducive environment for the video call: Ensure you have a quiet, well-lit space with a stable internet
+                connection for the video call. Having a suitable environment will facilitate a seamless verification process.
+              </p>
+              <p className={styles.para}>Await our call: After a specific timeframe, we will reach out to you via phone to initiate the verification process.</p>
+              <p className={styles.para}>Follow the instructions provided: During the call, our representative will guide you through the necessary steps to complete the verification successfully.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Contact_soon;
