1a5a44eed5700780c2d7d62df81909991bd17ccf
Niladri <rickyrickado14@gmail.com>
2023-06-17 17:20:45 +0530

    updated home page responsiveness

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -6,12 +6,8 @@ import Link from 'next/link';
 function Register() {
   return (
     <div className={styles.features}>
-      <Image src='/mac4.png' width={700} height={700} />
+      <Image src='/mac4.png' width={700} className={styles.image} height={700} />
       <div className={styles.container}>
-        {/* <div className={styles.texts}>
-          <h1 className={styles.heading}>Register For Free</h1>
-          <span style={{ fontSize: '2.4rem' }}>on </span><i style={{ fontSize: '3.2rem' }}>Fipezo</i>
-        </div> */}
         <div className={styles.btns}>
           <Link href='/register/freelancer' className={styles.btn}>Register as a Freelancer</Link>
           <Link href='/register/company' className={styles.btn}>Register as a Company</Link>
