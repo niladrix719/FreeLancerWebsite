d1637be87901713940866c094292056de8d8f2e9
Niladri <rickyrickado14@gmail.com>
2023-07-22 15:09:40 +0530

    routes changed for server

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: [`${process.env.CLIENT_URL}`, 'https://www.google.com', 'https://fipezo.vercel.app']
+  origin: [`${process.env.CLIENT_URL}`, 'https://fipezo.com', 'https://fipezo.com:3001', 'https://www.google.com', 'https://fipezo.vercel.app']
 }));
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
@@ -42,42 +42,42 @@ const userCollection = require('./models/userModel');
 const { getFileStream } = require('./middlewares/s3');
 
 // Setting up the routes
-app.post('/signup', signupController);
-app.post('/login', loginController);
-app.post('/otp', otpController);
-app.post('/otp/signup', otpSignupController);
-app.post('/register/freelancer', upload, verifyToken, registerFreelancer);
-app.post('/register/company', companyUpload, verifyToken, registerCompany);
-app.post('/verify/freelancer/phone', VerifyFreelancerPhone);
-app.post('/verify/company/phone', VerifyCompanyPhone);
-app.get('/profile/freelancer/:uid', getFreelancerProfile);
-app.get('/profiles/verified/freelancer', getFreelancerProfiles);
-app.get('/profiles/featured/freelancer', getFeaturedFreelancerProfiles);
-app.get('/profiles/unverified/freelancer', verifyToken, getUnFreelancerProfiles);
-app.get('/profiles/unverified/company', verifyToken, getUnCompanyProfiles);
-app.get('/contact/messages', verifyToken, fetchContactUs);
-app.delete('/delete/freelancer/:id', deleteFreelancerProfile);
-app.delete('/delete/company/:id', deleteCompanyProfileV);
-app.delete('/profile/user/delete', verifyToken, deleteUserProfile);
-app.delete('/profile/company/delete', verifyToken, deleteCompanyProfile);
-app.delete('/delete/request/:id', verifyToken, deleteRequest);
-app.put('/accept/request/:id', verifyToken, acceptRequest);
-app.put('/cancel/request/:id', verifyToken, cancelRequest);
-app.put('/verify/freelancer/:id', verifyFreelancerProfile);
-app.put('/verify/company/:id' , verifyCompanyProfile);
-app.post('/contact', contactUs);
-app.get('/profile/user', verifyToken, getUserProfile);
-app.get('/navbar', verifyToken, getNavbar);
-app.post('/add/review', verifyToken, addReview);
-app.get('/reviews/:id', getReviews);
-app.post('/add/hire', verifyToken, addHire);
-app.put('/profile/user/edit', userProfilePic, verifyToken, editUserProfile);
-app.put('/profile/company/edit', companyEditUpload, verifyToken, editCompanyProfile);
-app.get('/profile', verifyToken, getProfile);
-app.get('/hires', verifyToken, getHires);
-app.get('/requests', verifyToken, getRequests);
+app.post('/api/signup', signupController);
+app.post('/api/login', loginController);
+app.post('/api/otp', otpController);
+app.post('/api/otp/signup', otpSignupController);
+app.post('/api/register/freelancer', upload, verifyToken, registerFreelancer);
+app.post('/api/register/company', companyUpload, verifyToken, registerCompany);
+app.post('/api/verify/freelancer/phone', VerifyFreelancerPhone);
+app.post('/api/verify/company/phone', VerifyCompanyPhone);
+app.get('/api/profile/freelancer/:uid', getFreelancerProfile);
+app.get('/api/profiles/verified/freelancer', getFreelancerProfiles);
+app.get('/api/profiles/featured/freelancer', getFeaturedFreelancerProfiles);
+app.get('/api/profiles/unverified/freelancer', verifyToken, getUnFreelancerProfiles);
+app.get('/api/profiles/unverified/company', verifyToken, getUnCompanyProfiles);
+app.get('/api/contact/messages', verifyToken, fetchContactUs);
+app.delete('/api/delete/freelancer/:id', deleteFreelancerProfile);
+app.delete('/api/delete/company/:id', deleteCompanyProfileV);
+app.delete('/api/profile/user/delete', verifyToken, deleteUserProfile);
+app.delete('/api/profile/company/delete', verifyToken, deleteCompanyProfile);
+app.delete('/api/delete/request/:id', verifyToken, deleteRequest);
+app.put('/api/accept/request/:id', verifyToken, acceptRequest);
+app.put('/api/cancel/request/:id', verifyToken, cancelRequest);
+app.put('/api/verify/freelancer/:id', verifyFreelancerProfile);
+app.put('/api/verify/company/:id' , verifyCompanyProfile);
+app.post('/api/contact', contactUs);
+app.get('/api/profile/user', verifyToken, getUserProfile);
+app.get('/api/navbar', verifyToken, getNavbar);
+app.post('/api/add/review', verifyToken, addReview);
+app.get('/api/reviews/:id', getReviews);
+app.post('/api/add/hire', verifyToken, addHire);
+app.put('/api/profile/user/edit', userProfilePic, verifyToken, editUserProfile);
+app.put('/api/profile/company/edit', companyEditUpload, verifyToken, editCompanyProfile);
+app.get('/api/profile', verifyToken, getProfile);
+app.get('/api/hires', verifyToken, getHires);
+app.get('/api/requests', verifyToken, getRequests);
 
-app.get('/images/:key', async (req, res) => {
+app.get('/api/images/:key', async (req, res) => {
   const key = req.params.key;
   try {
     const readStream = await getFileStream(key);
@@ -87,10 +87,12 @@ app.get('/images/:key', async (req, res) => {
   }
 });
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello From Fipezo Server');
 });
 
+const ip = '0.0.0.0';
+
 // Starting the server
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, ip, () => console.log(`Server started on port ${port}`));
